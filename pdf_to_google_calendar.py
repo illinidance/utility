00f8e9bfa8d5a39ed349e49ca97d6c96d6f4ee8c
Jinyang Li <jinyang7@illinois.edu>
2022-05-16 11:58:16 -0500

    Imporve doc

--- pdf_to_google_calendar.py
+++ pdf_to_google_calendar.py
@@ -17,12 +17,20 @@ from pytz import timezone
 def parse_pdf(pdf_file: str) -> List[Dict]:
     text = extract_text(pdf_file)
 
+    # E.g.: "Tuesday, June 7, 2022"
     date_pattern = re.compile(r"\w+, \w+ \d+, \d{4}")
+    # E.g.: "7:00 PM - 10:00 PM Illini Dancesport Practice Space (Tentative 4/1/2022)"
     time_pattern = re.compile(r"\d+:\d+ [AP]M - \d+:\d+ [AP]M")
+    # TODO: Better localtion handling. Currently the location is hardcoded. The
+    # difficultly comes from line breaking. Sometimes the location, e.g., "314A"
+    # will be in the same line as the `time_pattern`. But sometime it's on a
+    # new line.
     location_pattern = re.compile(
         r"314A|314B|Ballroom|Room A\b|Room B\b|Room C\b|Multipurpose"
     )
 
+    # filter out noisy lines and only keep lines that contain the (1) date, (2)
+    # the time or (3) the location of an event.
     filtered_text = ""
     for s in text.splitlines():
         if date_pattern.match(s):
@@ -40,41 +48,46 @@ def parse_pdf(pdf_file: str) -> List[Dict]:
             filtered_text += s.strip()
     filtered_text = filtered_text.strip()
 
+    # E.g.: "Sunday, June 12, 2022 7:00 PM - 10:00 PM Illini Dancesport Practice Space (Tentative 4/1/2022) 314A"
+    #   date: Sunday, June 12, 2022
+    #   time: 7:00 PM - 10:00 PM
+    #   event_name: Illini Dancesport Practice Space
+    #   status: Tentative 4/1/2022
+    #   location: 314A
     pattern = re.compile(
         r"(?P<date>\w+, \w+ \d+, \d{4}) (?P<time>\d+:\d+ [AP]M - \d+:\d+ [AP]M) (?P<event_name>.*) \((?P<status>.*)\) (?P<location>.*)"
     )
 
-    events = [m.groupdict() for m in pattern.finditer(filtered_text)]
-
-    events = [make_event_dict(m) for m in events]
+    events = [make_event_dict(m.groupdict()) for m in pattern.finditer(filtered_text)]
     return events
+
+
+def to_localized_isoformat(m_date: str, m_time: str) -> Dict[str, str]:
+    """Conert time str to dict with ISO format time str and timezone info."""
+    time_fmt = "%I:%M %p, %A, %B %d, %Y"
+    # "7:00 PM, Tuesday, May 17, 2022"
+    tz = timezone("US/Central")
+    m_t = f"{m_date.strip()}, {m_time.strip()}"
+    m_t = datetime.datetime.strptime(m_t, time_fmt)
+    m_t = tz.localize(m_t)
+    # "2022-05-17T19:00:00-05:00"
+    m_t = m_t.isoformat()
+    # {"dateTime": "2022-05-17T19:00:00-05:00", "timeZone": "US/Central"}
+    return {"dateTime": m_t, "timeZone": str(tz)}
 
 
 def make_event_dict(m: Dict, location_prefix="Illini Union") -> Dict:
     """Convert the input dictionary into a form accepted by the Google Calendar API."""
     # start and end time
-    time_fmt = "%I:%M %p, %A, %B %d, %Y"
-    central = timezone("US/Central")
-
     start_time, end_time = tuple(m["time"].split("-"))
-    start_time = start_time.strip() + ", " + m["date"]
-    end_time = end_time.strip() + ", " + m["date"]
-    start_time = datetime.datetime.strptime(start_time, time_fmt)
-    end_time = datetime.datetime.strptime(end_time, time_fmt)
-    start_time = central.localize(start_time)
-    end_time = central.localize(end_time)
+    start_time = to_localized_isoformat(m["date"], start_time)
+    end_time = to_localized_isoformat(m["date"], end_time)
 
     return {
         "summary": f"{m['event_name']} ({m['status']})",
         "location": f"{location_prefix} {m['location']}",
-        "start": {
-            "dateTime": start_time.isoformat(),
-            "timeZone": str(central),
-        },
-        "end": {
-            "dateTime": end_time.isoformat(),
-            "timeZone": str(central),
-        },
+        "start": start_time,
+        "end": end_time,
     }
 
 
@@ -83,6 +96,7 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 
 def update_calendar(events: List[Dict]):
+    """Update the team's Google Calendar with given events."""
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -102,9 +116,7 @@ def update_calendar(events: List[Dict]):
 
     try:
         service = build("calendar", "v3", credentials=creds)
-
         for event in events:
-
             # check if it already exists
             existing_events = (
                 service.events()
