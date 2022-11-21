d4891d5f6a9e000648f07e7ec228aff18da83baa
Carl Edwards <cne2@illinois.edu>
2022-11-21 17:58:58 -0600

    Reformatted with black. Also, added classify song from file function.

--- song_assigner/get_youtube.py
+++ song_assigner/get_youtube.py
@@ -1,9 +1,9 @@
-
 from tempfile import TemporaryDirectory
 
 import subprocess
 
 import os.path as osp
+
 
 def parse_file_metadata(file):
 
@@ -12,14 +12,15 @@ def parse_file_metadata(file):
     with open(file) as f:
         for line in f:
             try:
-                if line == '\n': continue
-            
-                artist, name, url = line.strip().split('\t')
+                if line == "\n":
+                    continue
+
+                artist, name, url = line.strip().split("\t")
                 artists.append(artist)
                 names.append(name)
                 urls.append(url)
             except:
-                print('Error in line:', line)
+                print("Error in line:", line)
 
     return artists, names, urls
 
@@ -31,42 +32,47 @@ def parse_file(file):
     with open(file) as f:
         for line in f:
             try:
-                if line == '\n': continue
-            
-                tmp = line.strip().split('\t')
+                if line == "\n":
+                    continue
+
+                tmp = line.strip().split("\t")
                 for t in tmp:
-                    if t.startswith('https://') or t.startswith('www.'):
+                    if t.startswith("https://") or t.startswith("www."):
                         urls.append(t)
             except:
-                print('Error in line:', line)
+                print("Error in line:", line)
 
     return urls
 
 
 def download_youtube(urls, tmp_dir):
-    
+
     for i, url in enumerate(urls):
-        print('downloading #', i, 'with URL', url)
+        print("downloading #", i, "with URL", url)
         try:
-            subprocess.run(['youtube-dl', '--rm-cache-dir'])
-            subprocess.run(['youtube-dl', '-x', '--download-archive', osp.join(tmp_dir, 'downloaded.txt'), '--audio-format','mp3', '-o',osp.join(tmp_dir, '%(title)s.%(ext)s'), '--rm-cache-dir', url])
-        except: 
-            print(url, 'failed to download')
+            subprocess.run(["youtube-dl", "--rm-cache-dir"])
+            subprocess.run(
+                [
+                    "youtube-dl",
+                    "-x",
+                    "--download-archive",
+                    osp.join(tmp_dir, "downloaded.txt"),
+                    "--audio-format",
+                    "mp3",
+                    "-o",
+                    osp.join(tmp_dir, "%(title)s.%(ext)s"),
+                    "--rm-cache-dir",
+                    url,
+                ]
+            )
+        except:
+            print(url, "failed to download")
 
 
+if __name__ == "__main__":
 
+    urls = parse_file("example_song_list.txt")
 
-if __name__ == '__main__':
-    
-    urls = parse_file('example_song_list.txt')
-    #print(urls)
-    
     temp_dir = TemporaryDirectory()
-    
-    download_youtube(urls, 'tmp') #temp_dir.name)
-    
-    #print(temp_dir.name)
-    
-    
-    
-    
+
+    download_youtube(urls, "tmp")
